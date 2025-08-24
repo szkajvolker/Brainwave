799193e59803bf76a84a96c327d4652a7f3ac895
szkajvolker <ist9595@windowslive.com>
2025-08-24 17:29:01 +0200

    Implementing Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
-      <Header />
-      <ButtonGradient />
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <ButtonGradient />
+      </div>
     </>
   );
 };
