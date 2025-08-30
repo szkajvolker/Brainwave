cf8bd12a9f87b472265c4f10cfcad202bfd29ae1
szkajvolker <ist9595@windowslive.com>
2025-08-30 13:54:42 +0200

    footer typo fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import Section from "./Section";
 const Footer = () => {
   return (
     <Section crosses className="!px-0 !py-10">
-      <div className="container flex sm:justify-between  items.center gap-10 max-sm:flex-col">
+      <div className="container flex sm:justify-between items-center gap-10 max-sm:flex-col">
         <p className="caption text-n-4 lg:block">
           © {new Date().getFullYear()}. All rights reserved.
         </p>
